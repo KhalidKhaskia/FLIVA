9d0c6168bcfe5aadde8e3dd7fd61f1bde7ce8946
KhalidKhaskia <khalid.khaskia@gmail.com>
2020-07-22 09:04:09 +0300

    call system with sending sms to the user and run the gps sensor

--- FLIVA System/app.py
+++ FLIVA System/app.py
@@ -3,11 +3,31 @@ from Sensors_Activating import Activate_Sensors as ActS
 #from flask_api import FlaskAPI
 
 app=Flask(__name__)
-    
+   
+#this method is just for testing 
 @app.route('/',methods=["GET"])  
 def getMethod():    
     return "This is GET method" 
 
+#the get method that activate the sensor manualy to get an indication if there any one stuck in the car
+@app.route('/ActivateSensors',methods=["GET"])  
+def getMethod2():   
+    opj = ActS()
+    opj.Activate()
+    print(opj.SensorJson)
+    res=jsonify(opj.SensorJson)
+    return res
+    
+#A test post method 
+@app.route('/TestPostApp',methods=["POST"])  
+def postMethod2():   
+    opj = ActS()
+    opj.Activate()
+    print(opj.SensorJson)
+    res=jsonify(opj.SensorJson)
+    return res
+    
+#a post method the return a jason object to the app
 @app.route('/sensors',methods=["POST"])  
 def postMethod():
     print(request)
@@ -23,21 +43,6 @@ def postMethod():
     else:    
         return "laa hawla wlaa qoaa ilaa bellah"
 
-@app.route('/ActivateSensors',methods=["GET"])  
-def getMethod2():   
-    opj = ActS()
-    opj.Activate()
-    print(opj.SensorJson)
-    res=jsonify(opj.SensorJson)
-    return res
-    
-@app.route('/TestPostApp',methods=["POST"])  
-def postMethod2():   
-    opj = ActS()
-    opj.Activate()
-    print(opj.SensorJson)
-    res=jsonify(opj.SensorJson)
-    return res
 
 if __name__=='__main__':
     app.run(debug=True ,host='192.168.68.116')
